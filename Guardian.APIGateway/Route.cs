2dba393654a47faf5f083b80c192533e057f66e0
young_developman <nazarmazurik99@gmail.com>
2020-02-12 22:06:30 +0200

    [WIP] issue/4: Add route class

---
+++ Guardian.APIGateway/Route.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardian.APIGateway
+{
+    public class Route
+    {
+        public string Endpoint { get; set; }
+        public DestinationResource Destination { get; set; }
+    }
+}
