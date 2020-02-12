abb204139f97cb2fb72f2aa175e31e30fbef24d5
young_developman <nazarmazurik99@gmail.com>
2020-02-12 22:08:27 +0200

    [WIP] issue/4: Add router class

---
+++ Guardian.APIGateway/Router.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardian.APIGateway
+{
+    public class Router
+    {
+
+        public List<Route> Routes { get; set; }
+        public DestinationResource AuthenticationService { get; set; }
+
+
+        public Router(string routeConfigFilePath)
+        {
+            dynamic router = JsonLoader.LoadFromFile<dynamic>(routeConfigFilePath);
+
+            Routes = JsonLoader.Deserialize<List<Route>>(Convert.ToString(router.routes));
+            AuthenticationService = JsonLoader.Deserialize<DestinationResource>(Convert.ToString(router.authenticationService));
+
+        }
+    }
+}
