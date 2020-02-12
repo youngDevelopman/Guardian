71bf691a90d9c4c9885361d780968083993caf19
young_developman <nazarmazurik99@gmail.com>
2020-02-12 22:09:53 +0200

    [WIP] issue/4: Add route request method to the router

--- Guardian.APIGateway/Router.cs
+++ Guardian.APIGateway/Router.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Guardian.APIGateway
@@ -20,5 +24,41 @@ namespace Guardian.APIGateway
             AuthenticationService = JsonLoader.Deserialize<DestinationResource>(Convert.ToString(router.authenticationService));
 
         }
+        public async Task<HttpResponseMessage> RouteRequest(HttpRequest request)
+        {
+            string path = request.Path.ToString();
+            string basePath = '/' + path.Split('/')[1];
+
+            DestinationResource destination;
+            try
+            {
+                destination = Routes.First(r => r.Endpoint.Equals(basePath)).Destination;
+            }
+            catch
+            {
+                return ConstructErrorMessage("The path could not be found.");
+            }
+
+            if (destination.RequiresAuthentication)
+            {
+                string token = request.Headers["token"];
+                request.Query.Append(new KeyValuePair<string, StringValues>("token", new StringValues(token)));
+                HttpResponseMessage authResponse = await AuthenticationService.SendRequest(request);
+                if (!authResponse.IsSuccessStatusCode) return ConstructErrorMessage("Authentication failed.");
+            }
+
+            return await destination.SendRequest(request);
+        }
+
+        private HttpResponseMessage ConstructErrorMessage(string error)
+        {
+            HttpResponseMessage errorMessage = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent(error)
+            };
+            return errorMessage;
+        }
+
     }
 }
