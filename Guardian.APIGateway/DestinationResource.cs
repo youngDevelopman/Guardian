82049fbe170a6c031186f5b61ab7fccf98556a99
young_developman <nazarmazurik99@gmail.com>
2020-02-16 23:26:18 +0200

    [WIP] issue/1: Merge with master

---
+++ Guardian.APIGateway/DestinationResource.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guardian.APIGateway
+{
+    public class DestinationResource
+    {
+        public string Uri { get; set; }
+        public bool RequiresAuthentication { get; set; }
+
+        static HttpClient client = new HttpClient();
+
+        public DestinationResource(string uri, bool requiresAuthentication)
+        {
+            Uri = uri;
+            RequiresAuthentication = requiresAuthentication;
+        }
+
+        public DestinationResource(string uri)
+           : this(uri, false)
+        {
+        }
+
+        private DestinationResource()
+        {
+            Uri = "/";
+            RequiresAuthentication = false;
+        }
+
+        public async Task<HttpResponseMessage> SendRequest(HttpRequest request)
+        {
+            string requestContent;
+            using (Stream receiveStream = request.Body)
+            {
+                using (StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8))
+                {
+                    requestContent = await readStream.ReadToEndAsync();
+                }
+            }
+
+            using (var newRequest = new HttpRequestMessage(new HttpMethod(request.Method), this.Uri))
+            {
+                newRequest.Content = new StringContent(requestContent, Encoding.UTF8, request.ContentType);
+                var response = await client.SendAsync(newRequest);
+                return response;
+            }
+        }
+    }
+}
