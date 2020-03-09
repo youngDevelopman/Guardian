2a9401db6d9b8f48829388a3c12d2b57f515467c
young_developman <nazarmazurik99@gmail.com>
2020-03-09 18:27:54 +0200

    [WIP] issue/19: Add project template.

---
+++ Guardian.ResourceService/Controllers/ResourceController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guardian.ResourceService.Controllers
+{
+    public class ResourceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
