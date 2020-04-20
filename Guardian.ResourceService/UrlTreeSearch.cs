857ad699bfff9ffe9a979c44655432aaa039f1f1
nazar.mazuryk <nazar.mazuryk@stryker.com>
2020-04-20 18:23:12 +0300

    [WIP] issue/2: Add proxy integration to the BFS algorithm

--- Guardian.ResourceService/UrlTreeSearch.cs
+++ Guardian.ResourceService/UrlTreeSearch.cs
@@ -7,9 +7,10 @@ namespace Guardian.ResourceService
 {
     public class UrlTreeSearch
     {
+        private const string proxyString = "{proxy+}";
         public void IsPathExists(List<ResourceModel> resources, List<string> paths)
         {
-            var result = this.BFS(resources, paths);
+            var result = this.BFS(resources, paths).ToList();
         }
 
         private Queue<ResourceModel> BFS(List<ResourceModel> resources, List<string> paths)
@@ -27,11 +28,39 @@ namespace Guardian.ResourceService
                 if (currentSegment.Endpoint == paths[pathCounter])
                 {
                     resultQueue.Enqueue(currentSegment);
+                    queue.Clear();
+                    
+                    var proxyElement = currentSegment.ResourceBranches.Find(x => x.Endpoint == proxyString);
                     foreach (var adjElement in currentSegment.ResourceBranches)
                     {
-                        queue.Enqueue(adjElement);
+                        if(adjElement != proxyElement)
+                        {
+                            queue.Enqueue(adjElement);
+                        }
+                    }
+                    
+                    if(proxyElement != null)
+                    {
+                        queue.Append(proxyElement);
                     }
                     pathCounter++;
+                }
+                else if(queue.Count == 0 && paths[pathCounter] == proxyString)
+                {
+                    var proxySegment = currentSegment;
+                    string leftPath = string.Empty;
+                    for (int i = pathCounter; i < paths.Count; i++)
+                    {
+                        leftPath += paths[i];
+                    }
+                    proxySegment.Endpoint = leftPath;
+                    
+                    resultQueue.Enqueue(currentSegment);
+                    queue.Clear();
+                }
+                else if(queue.Count == 0)
+                {
+                    throw new Exception("Exact path is not found");
                 }
             }
 
