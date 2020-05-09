6ab67c82427136795ee5173256d3501cf30cd924
nazar.mazuryk <nazar.mazuryk@stryker.com>
2020-05-09 20:43:00 +0300

    issue/28: Add required attributes for the models.

--- Guardian.Shared/Models/GetResourceRequest.cs
+++ Guardian.Shared/Models/GetResourceRequest.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Guardian.Shared.Models
 {
     public class GetResourceRequest
     {
+        [Required]
         public string Domain { get; set; }
 
+        [Required]
         public string RelativePath { get; set; }
     }
 }
--- Guardian.Shared/Models/TokenValidationRequest.cs
+++ Guardian.Shared/Models/TokenValidationRequest.cs
@@ -1,13 +1,16 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Guardian.Shared.Models
 {
     public class TokenValidationRequest
     {
+        [Required]
         [JsonProperty("access_token")]
         public string AccessToken { get; set; }
 
+        [Required]
         public Guid UserPoolId { get; set; }
     }
 }
